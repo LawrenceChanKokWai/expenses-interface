a69fbe6dd106578d6606343767d0f3954fb9b393
Kok Wai Chan <kokwai2107@gmail.com>
2021-07-07 21:56:22 +0800

    Updated Spitting Component of Date

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,15 +1,10 @@
 import "./ExpensesItem.css";
+import ExpensesDate from "./ExpensesDate";
 
 function ExpensesItem(props) {
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
   return (
     <div className="expense-item">
-      <div>{year}</div>
-      <div>{month}</div>
-      <div>{day}</div>
+      <ExpensesDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
